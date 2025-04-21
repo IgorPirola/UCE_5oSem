5f0d4c1b174d9c2a0aac0ef2bff9fdad1a7af6fc
Igor M. Pirola <igormarianopirola@gmail.com>
2025-04-21 17:37:10 -0300

    :sparkles: feat
    + botão de seleção da dificuldade com dropdown menu

--- main.lua
+++ main.lua
@@ -1,8 +1,13 @@
 LG = love.graphics
+LK = love.keyboard
 
+-- Cores
 local black = {0, 0, 0, 1}
 local white = {1, 1, 1, 1}
+
+-- Variaveis Globais
 MenuPrincipal = {posX=0, posY=0, canvas=nil}
+BtnDificuldade = {ativado=false, txt="", width=0, height=0}
 
 function love.load()
     Fonte = LG.newFont('src/Inter-VariableFont_opsz,wght.ttf', 22)
@@ -24,12 +29,19 @@ function love.load()
         LG.setBlendMode("alpha")
     LG.setCanvas()
 
+    -- BtnDificuldade
+    BtnDificuldade.txt = "Selecione a dificuldade"
+    BtnDificuldade.width = Fonte2:getWidth(BtnDificuldade.txt)
+    BtnDificuldade.height = Fonte2:getHeight(BtnDificuldade.txt)
+
     -- Testes
     printx = 0
     printy = 0
+    botao = ""
 end
 
 function love.draw()
+    LG.print("" .. tostring(love.timer.getFPS()), 0,0)
     LG.setBackgroundColor(white)
     LG.setFont(Fonte)
 
@@ -43,45 +55,99 @@ function love.draw()
     LG.print("Partidas Jogadas: ", MenuPrincipal.posX+15, MenuPrincipal.posY+70)
     LG.print("Acertos: ", MenuPrincipal.posX+15, MenuPrincipal.posY+110)
     LG.print("Erros: ", MenuPrincipal.posX+15, MenuPrincipal.posY+150)
+
+    -- Separador
     LG.line(MenuPrincipal.posX+1, MenuPrincipal.posY+230, MenuPrincipal.posX+CanvasX-1, MenuPrincipal.posY+230)
+
     LG.print("Jogar", MeioX-((Fonte:getWidth("Jogar"))/2), MenuPrincipal.posY+245)
+    
+    if not BtnDificuldade.ativado then
+        -- Botões
+        LG.setColor(white)
+        LG.rectangle("fill", MeioX-125,MenuPrincipal.posY+400, 250, 65)
+        LG.rectangle("fill", MeioX-125,MenuPrincipal.posY+515, 250, 65)
+        LG.rectangle("fill", MeioX-125,MenuPrincipal.posY+630, 250, 65)
 
+        -- Texto dos botões
+        LG.setFont(Fonte)
+        LG.setColor(black)
+        LG.print("Novo Jogo", MeioX-((Fonte:getWidth("Novo Jogo"))/2), MenuPrincipal.posY+400+32.5-((Fonte:getHeight("Novo Jogo"))/2))
+        LG.print("Continuar", MeioX-((Fonte:getWidth("Continuar"))/2), MenuPrincipal.posY+515+32.5-((Fonte:getHeight("Continuar"))/2))
+        LG.print("Sair", MeioX-((Fonte:getWidth("Sair"))/2), MenuPrincipal.posY+630+32.5-((Fonte:getHeight("Sair"))/2))
+    end
+
+    -- Botão dificuldade | Dropdown
     LG.setFont(Fonte2)
-    LG.print("Selecione a dificuldade", MeioX-((Fonte2:getWidth("Selecione a dificuldade"))/2), MenuPrincipal.posY+320)
-    
-    -- Botões
-    LG.setColor(white)
-    LG.rectangle("fill", MeioX-125,MenuPrincipal.posY+400, 250, 65)
-    LG.rectangle("fill", MeioX-125,MenuPrincipal.posY+515, 250, 65)
-    LG.rectangle("fill", MeioX-125,MenuPrincipal.posY+630, 250, 65)
+    LG.print(BtnDificuldade.txt, MeioX-BtnDificuldade.width/2-Fonte2:getWidth(" v"), MenuPrincipal.posY+320)
+    LG.print(" v", MeioX+BtnDificuldade.width/2, MenuPrincipal.posY+320)
+    LG.line(MeioX-BtnDificuldade.width/2-Fonte2:getWidth(" v"), MenuPrincipal.posY+320+BtnDificuldade.height, MeioX+BtnDificuldade.width/2+Fonte2:getWidth(" v"), MenuPrincipal.posY+320+BtnDificuldade.height)
+    if BtnDificuldade.ativado then
+        LG.setColor(white)
+        LG.rectangle("fill", MeioX-BtnDificuldade.width/2, MenuPrincipal.posY+320+BtnDificuldade.height+5, BtnDificuldade.width, BtnDificuldade.height+75)
+        LG.setColor(black)
 
-    -- Texto dos botões
-    LG.setFont(Fonte)
-    LG.setColor(black)
-    LG.print("Novo Jogo", MeioX-((Fonte:getWidth("Novo Jogo"))/2), MenuPrincipal.posY+400+32.5-((Fonte:getHeight("Novo Jogo"))/2))
-    LG.print("Continuar", MeioX-((Fonte:getWidth("Continuar"))/2), MenuPrincipal.posY+515+32.5-((Fonte:getHeight("Continuar"))/2))
-    LG.print("Sair", MeioX-((Fonte:getWidth("Sair"))/2), MenuPrincipal.posY+630+32.5-((Fonte:getHeight("Sair"))/2))
-    
+        LG.print("Fácil", MeioX-(Fonte2:getWidth("Fácil")/2), MenuPrincipal.posY+320+BtnDificuldade.height+10)
+        LG.line(MeioX-(Fonte2:getWidth("Fácil")/2)-20, MenuPrincipal.posY+320+BtnDificuldade.height+10+Fonte2:getHeight("Fácil")+2.5, MeioX+(Fonte2:getWidth("Fácil")/2)+20, MenuPrincipal.posY+320+BtnDificuldade.height+10+Fonte2:getHeight("Fácil")+2.5)
+
+        LG.print("Médio", MeioX-(Fonte2:getWidth("Médio")/2), MenuPrincipal.posY+320+BtnDificuldade.height+40)
+        LG.line(MeioX-(Fonte2:getWidth("Fácil")/2)-20, MenuPrincipal.posY+320+BtnDificuldade.height+40+Fonte2:getHeight("Fácil")+2.5, MeioX+(Fonte2:getWidth("Fácil")/2)+20, MenuPrincipal.posY+320+BtnDificuldade.height+40+Fonte2:getHeight("Fácil")+2.5)
+
+        LG.print("Difícil", MeioX-(Fonte2:getWidth("Difícil")/2), MenuPrincipal.posY+320+BtnDificuldade.height+70)
+    end
     -- Testes
-    love.graphics.print("Click", printx, printy)
+    LG.print(botao, printx, printy)
 end
 
 function love.update(dt)
-    
+    if LK.isDown('escape') then
+        love.event.quit()
+    end
 end
 
 function love.mousepressed(x, y, button, istouch)
     if button == 1 then	
-        if x >= MeioX-125 and x <= MeioX-125+250 and y >= MenuPrincipal.posY+400 and y <= MenuPrincipal.posY+400+65 then
-            printx = x
-            printy = y
-        end
-        if x >= MeioX-125 and x <= MeioX-125+250 and y >= MenuPrincipal.posY+515 and y <= MenuPrincipal.posY+515+65 then
-            printx = x
-            printy = y
-        end
-        if x >= MeioX-125 and x <= MeioX-125+250 and y >= MenuPrincipal.posY+630 and y <= MenuPrincipal.posY+630+65 then
-            love.event.quit()
+        if not BtnDificuldade.ativado then
+            if x >= MeioX-BtnDificuldade.width/2-5 and x <= MeioX+BtnDificuldade.width/2+5 and y >= MenuPrincipal.posY+320-5 and y <= MenuPrincipal.posY+320+BtnDificuldade.height+5 then
+                BtnDificuldade.ativado = true
+            end
+
+            -- Novo Jogo
+            if x >= MeioX-125 and x <= MeioX-125+250 and y >= MenuPrincipal.posY+400 and y <= MenuPrincipal.posY+400+65 then
+                printx = x
+                printy = y
+                botao = "Novo Jogo"
+            end
+
+            -- Continuar
+            if x >= MeioX-125 and x <= MeioX-125+250 and y >= MenuPrincipal.posY+515 and y <= MenuPrincipal.posY+515+65 then
+                printx = x
+                printy = y
+                botao = "Continuar"
+            end
+
+            -- Sair
+            if x >= MeioX-125 and x <= MeioX-125+250 and y >= MenuPrincipal.posY+630 and y <= MenuPrincipal.posY+630+65 then
+                love.event.quit()
+            end
+        else
+            if x >= MeioX-BtnDificuldade.width and x <= MeioX+BtnDificuldade.width and y >= MenuPrincipal.posY+320+BtnDificuldade.height+5 and y <= MenuPrincipal.posY+320+BtnDificuldade.height+100 then
+                if x >= MeioX-BtnDificuldade.width and x <= MeioX+BtnDificuldade.width and y >= MenuPrincipal.posY+320+BtnDificuldade.height+10 and y <= MenuPrincipal.posY+320+BtnDificuldade.height+10+Fonte2:getHeight("Fácil") then
+                    BtnDificuldade.txt = "Fácil"
+                    BtnDificuldade.ativado = false
+                end
+                if x >= MeioX-BtnDificuldade.width and x <= MeioX+BtnDificuldade.width and y >= MenuPrincipal.posY+320+BtnDificuldade.height+40 and y <= MenuPrincipal.posY+320+BtnDificuldade.height+40+Fonte2:getHeight("Médio") then
+                    BtnDificuldade.txt = "Médio"
+                    BtnDificuldade.ativado = false
+                end
+                if x >= MeioX-BtnDificuldade.width and x <= MeioX+BtnDificuldade.width and y >= MenuPrincipal.posY+320+BtnDificuldade.height+70 and y <= MenuPrincipal.posY+320+BtnDificuldade.height+70+Fonte2:getHeight("Difícil")+5 then
+                    BtnDificuldade.txt = "Difícil"
+                    BtnDificuldade.ativado = false
+                end
+            else
+                if x >= MenuPrincipal.posX and y >= MenuPrincipal.posY then
+                    BtnDificuldade.ativado = false
+                end
+            end
         end
     end
-  end
+end
