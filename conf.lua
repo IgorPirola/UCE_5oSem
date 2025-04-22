7471d40ef76e44a651d6cd83270791f6b03e0e43
Igor M. Pirola <igormarianopirola@gmail.com>
2025-04-22 20:32:15 -0300

    :wrench: chore
    + novos parametros no conf para maior compatibilidade em dispositivos mais antigos

--- conf.lua
+++ conf.lua
@@ -2,4 +2,11 @@ function love.conf(t)
   t.title = "Perguntados Programação"
   t.window.width = 480
   t.window.height = 900
+
+  t.modules.graphics = true
+  t.window.opengl = true
+  t.gammacorrect = false
+
+  t.window.highdpi = false
+  t.window.msaa = 0
 end
